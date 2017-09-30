54e33480e63adf6a576d2ef47536c299179490e0
SiimKinks <kinkssiim@gmail.com>
2017-09-30 17:30:25 +0300

    Prepare for release 0.15.0

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ buildscript {
   }
   dependencies {
     classpath 'com.android.tools.build:gradle:<latest version>'
-    classpath 'com.siimkinks.sqlitemagic:sqlitemagic-plugin:0.14.0'
+    classpath 'com.siimkinks.sqlitemagic:sqlitemagic-plugin:0.15.0'
   }
 }
 
--- gradle-plugin/src/main/kotlin/com/siimkinks/sqlitemagic/SqliteMagicPlugin.kt
+++ gradle-plugin/src/main/kotlin/com/siimkinks/sqlitemagic/SqliteMagicPlugin.kt
@@ -12,7 +12,7 @@ import org.gradle.api.artifacts.DependencyResolutionListener
 import org.gradle.api.artifacts.DependencySet
 import org.gradle.api.artifacts.ResolvableDependencies
 
-const val VERSION = "0.15.0-SNAPSHOT"
+const val VERSION = "0.15.0"
 
 class SqliteMagicPlugin : Plugin<Project> {
   override fun apply(project: Project) {
